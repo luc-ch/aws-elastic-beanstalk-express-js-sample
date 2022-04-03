fb737d7ba7a7eb434313a5bcc5e358592711cdb3
Luciano Chamorro <lchamorro@fi.uba.ar>
2022-04-03 20:42:34 -0300

    Modify message for review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Primer deploy de Lucho'));
+app.get('/', (req, res) => res.send('Primer deploy de Lucho. Antes del despliegue, pasar por un review.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
